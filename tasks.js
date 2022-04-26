8ac014ad583bd26f599e7dccbc485292c14f886a
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2022-04-26 18:59:35 +0700

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -45,3 +45,17 @@ const rps = (p1, p2) => {
   }
   return res;
 };
+
+// --- task 3 ---
+
+/* Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that! */
+
+function removeEveryOther(arr){
+  //your code here
+}
