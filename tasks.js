599ab37237c5cc61cb1df350c5ebcc15cb6e06a5
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2022-05-13 14:46:44 +0700

    added task 7

--- tasks.js
+++ tasks.js
@@ -117,3 +117,13 @@ function longest(s1, s2) {
   }
   return out;
 }
+
+// --- task 7 ---
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(numbers) {
+
+}
