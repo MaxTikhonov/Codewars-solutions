78e70ea7a449410b58efeca3b519e40c91a2a0ae
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2022-05-13 15:16:40 +0700

    added description of task 8

--- tasks.js
+++ tasks.js
@@ -131,3 +131,17 @@ function squareSum(numbers) {
     return previousValue + currentValue;
   });
 }
+
+// --- task 8 ---
+
+// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+// Example: (Input --> Output)
+
+// "Dermatoglyphics" --> true
+// "aba" --> false
+// "moOse" --> false (ignore letter case)
+
+function isIsogram(str) {
+  //...
+}
