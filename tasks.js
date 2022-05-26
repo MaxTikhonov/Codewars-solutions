5245ab39647db09f9b309f40925e33ba8d70ba9d
MaxTikhonov <69316402+MaxTikhonov@users.noreply.github.com>
2022-05-26 16:48:29 +0700

    added 10 task

--- tasks.js
+++ tasks.js
@@ -190,3 +190,20 @@ function oddOrEven(array) {
     return "odd";
   }
 }
+
+// --- task 10 ---
+
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples:
+
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+
+var number = function (array) {
+  //your awesome code here
+}
